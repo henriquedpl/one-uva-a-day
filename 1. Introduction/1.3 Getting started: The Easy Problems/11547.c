a099677b1c7d2a0d8e54ef2d514a977da9f8c0a7
Henrique Lopes <oprometeumoderno@gmail.com>
2026-08-10 21:59:16 -0300

    add problem 11547

---
+++ 1. Introduction/1.3 Getting started: The Easy Problems/11547.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+  Problem 11547	Automatic Answer
+  Runtime: 0.000s
+*/
+
+int main(int argc, char *argv){
+  int cases, n;
+  scanf("%d", &cases);
+  while(cases-- > 0){
+    scanf("%d", &n);
+    printf("%d\n", abs(((((((((n * 567) / 9) + 7492) * 235) / 47) - 498) / 10) % 10)));
+  }
+  return 0;
+}
